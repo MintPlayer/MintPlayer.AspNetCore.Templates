b25485dea63a90007051e533f7c458997de9fc43
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-04-28 01:43:21 +0200

    Correct mistakes

--- src/content/IdentityProvider/Web/ClientApp/src/app/pages/account/register/register.module.ts
+++ src/content/IdentityProvider/Web/ClientApp/src/app/pages/account/register/register.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BsAlertModule } from '@mintplayer/ng-bootstrap/alert';
 import { BsForModule } from '@mintplayer/ng-bootstrap/for';
+import { BsFormModule } from '@mintplayer/ng-bootstrap/form';
 import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
 import { BsButtonTypeModule } from '@mintplayer/ng-bootstrap/button-type';
 
@@ -19,6 +20,7 @@ import { RegisterComponent } from './register.component';
 		FormsModule,
 		BsAlertModule,
 		BsForModule,
+		BsFormModule,
 		BsGridModule,
 		BsButtonTypeModule,
 		RegisterRoutingModule
