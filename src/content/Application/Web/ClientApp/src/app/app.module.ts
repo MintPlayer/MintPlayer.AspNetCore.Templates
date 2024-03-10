5c70f3853a06d279e22f0300db6176f22666b662
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-03-10 14:08:38 +0100

    Temp commit

--- src/content/Application/Web/ClientApp/src/app/app.module.ts
+++ src/content/Application/Web/ClientApp/src/app/app.module.ts
@@ -3,49 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxsModule } from '@ngxs/store';
-import { BsNavbarModule } from '@mintplayer/ng-bootstrap/navbar';
 import { AdvancedRouterModule } from '@mintplayer/ng-router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApplicationManager } from './states/application/application.manager';
-////#if (UsePwa)
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-////#endif
 
 @NgModule({
 	declarations: [
 		AppComponent
 	],
 	imports: [
-////#if (UseServerSideRendering)
-		BrowserModule.withServerTransition({ appId: 'serverApp' }),
-////#else
-		BrowserModule,
-////#endif
-		BrowserAnimationsModule,
-		HttpClientModule,
-////#if (UseXsrfProtection)
-		HttpClientXsrfModule.withOptions({
-			cookieName: 'XSRF-TOKEN',
-			headerName: 'X-XSRF-TOKEN'
-		}),
-////#endif
-		NgxsModule.forRoot([
-			ApplicationManager
-		]),
 		AdvancedRouterModule,
-		BsNavbarModule,
-////#if (UsePwa)
-		ServiceWorkerModule.register('ngsw-worker.js', {
-			enabled: environment.production,
-			//// Register the ServiceWorker as soon as the application is stable
-			//// or after 30 seconds (whichever comes first).
-			registrationStrategy: 'registerWhenStable:30000'
-		}),
-////#endif
-		AppRoutingModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
