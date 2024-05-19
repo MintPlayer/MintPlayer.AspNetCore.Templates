2dba0cd0dde2628bd18db135bb0057613f744129
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-05-19 12:00:37 +0200

    Temp commit

--- src/content/NewTemplate/Web/Program.cs
+++ src/content/NewTemplate/Web/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world");
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+app.Run();
