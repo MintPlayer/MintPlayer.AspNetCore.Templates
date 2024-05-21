bec2e9eeced5b6fab639a9a471e359411c1fb9df
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-05-21 20:03:52 +0200

    Add main

--- src/content/NewTemplate/Web/Program.cs
+++ src/content/NewTemplate/Web/Program.cs
@@ -1,28 +1,38 @@
 ﻿using MintPlayer.AspNetCore.SpaServices.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-builder.Services.AddSpaStaticFilesImproved(configuration => {
-    // In production, the Angular files will be served from this directory
-    configuration.RootPath = "ClientApp/dist";
-});
-builder.Services.AddSpaPrerenderingService<MintPlayer.AspNetCore.Template.Services.SpaPrerenderingService>();
+namespace MintPlayer.AspNetCore.Template.Services;
 
-var app = builder.Build();
-app.UseStaticFiles();
-if (!env.IsDevelopment()) {
-    app.Environment.UseSpaStaticFilesImproved();
-}
-app.UseAuthentication();
-app.UseRouting();
-app.UseAuthorization();
-app.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
-app.UseSpaImproved(spa => {
-    spa.Options.SourcePath = "ClientApp";
-    // For angular 17
-    spa.Options.CliRegexes = [new Regex(@"Local\:\s+(?<openbrowser>https?\:\/\/(.+))")];
-    if (env.IsDevelopment()) {
-        spa.UseAngularCliServer(npmScript: "start");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddAuthentication();
+        builder.Services.AddAuthorization();
+        builder.Services.AddSpaStaticFilesImproved(configuration => {
+            // In production, the Angular files will be served from this directory
+            configuration.RootPath = "ClientApp/dist";
+        });
+        builder.Services.AddSpaPrerenderingService<MintPlayer.AspNetCore.Template.Services.SpaPrerenderingService>();
+
+        var app = builder.Build();
+        app.UseStaticFiles();
+        if (!env.IsDevelopment()) {
+            app.Environment.UseSpaStaticFilesImproved();
+        }
+        app.UseAuthentication();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
+        app.UseSpaImproved(spa => {
+            spa.Options.SourcePath = "ClientApp";
+            // For angular 17
+            spa.Options.CliRegexes = [new Regex(@"Local\:\s+(?<openbrowser>https?\:\/\/(.+))")];
+            if (env.IsDevelopment()) {
+                spa.UseAngularCliServer(npmScript: "start");
+            }
+        });
+        app.Run();
     }
-});
-app.Run();
+}
