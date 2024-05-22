d51e39c27adb61933f6f5f60afc942f5429e85d8
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-05-22 06:05:44 +0200

    Add package

--- src/content/NewTemplate/Data/Extensions/ApplicationExtensions.cs
+++ src/content/NewTemplate/Data/Extensions/ApplicationExtensions.cs
@@ -1,19 +1,19 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using MintPlayer.AspNetCore.Template.Data.Abstractions.Access.Repositories;
-using MintPlayer.AspNetCore.Template.Data.Abstractions.Access.Services;
-using MintPlayer.AspNetCore.Template.Data.Access.Repositories;
-using MintPlayer.AspNetCore.Template.Data.Access.Services;
-using MintPlayer.AspNetCore.Template.Data.Access.Mappers;
+// using MintPlayer.AspNetCore.Template.Data.Abstractions.Access.Repositories;
+// using MintPlayer.AspNetCore.Template.Data.Abstractions.Access.Services;
+// using MintPlayer.AspNetCore.Template.Data.Access.Repositories;
+// using MintPlayer.AspNetCore.Template.Data.Access.Services;
+// using MintPlayer.AspNetCore.Template.Data.Access.Mappers;
 
 namespace MintPlayer.AspNetCore.Template.Data.Extensions;
 
 public static class ApplicationExtensions
 {
-	public static IServiceCollection AddApplication(this IServiceCollection services, Action<Options.ApplicationOptions> options)
+	public static IServiceCollection AddMeteo(this IServiceCollection services, Action<Options.MeteoOptions> options)
 	{
-		var op = new Options.ApplicationOptions(); options(op);
+		var op = new Options.MeteoOptions(); options(op);
 
 		services
 			.AddDbContext<Persistance.MeteoContext>(options => options.UseSqlServer(op.ConnectionString));
